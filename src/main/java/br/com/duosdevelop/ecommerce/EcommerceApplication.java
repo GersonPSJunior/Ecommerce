e7497770d7e761feb8881f51d0bd6b742cf5465c
Gerson Junior <gersonjunior1771@gmail.com>
2019-09-25 12:39:13 -0300

    Profile de test

--- src/main/java/br/com/duosdevelop/ecommerce/EcommerceApplication.java
+++ src/main/java/br/com/duosdevelop/ecommerce/EcommerceApplication.java
@@ -1,65 +1,11 @@
 package br.com.duosdevelop.ecommerce;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.duosdevelop.ecommerce.domain.Address;
-import br.com.duosdevelop.ecommerce.domain.CardPayment;
-import br.com.duosdevelop.ecommerce.domain.Category;
-import br.com.duosdevelop.ecommerce.domain.City;
-import br.com.duosdevelop.ecommerce.domain.Customer;
-import br.com.duosdevelop.ecommerce.domain.ItemPedido;
-import br.com.duosdevelop.ecommerce.domain.Payment;
-import br.com.duosdevelop.ecommerce.domain.PaymentSlip;
-import br.com.duosdevelop.ecommerce.domain.Pedido;
-import br.com.duosdevelop.ecommerce.domain.Product;
-import br.com.duosdevelop.ecommerce.domain.State;
-import br.com.duosdevelop.ecommerce.domain.enums.StatePayment;
-import br.com.duosdevelop.ecommerce.domain.enums.TypeCustomer;
-import br.com.duosdevelop.ecommerce.repositories.AddressRepository;
-import br.com.duosdevelop.ecommerce.repositories.CategoryRepository;
-import br.com.duosdevelop.ecommerce.repositories.CityRepository;
-import br.com.duosdevelop.ecommerce.repositories.CustomerRepository;
-import br.com.duosdevelop.ecommerce.repositories.ItemPedidoRepository;
-import br.com.duosdevelop.ecommerce.repositories.PaymentRepository;
-import br.com.duosdevelop.ecommerce.repositories.PedidoRepository;
-import br.com.duosdevelop.ecommerce.repositories.ProductRepository;
-import br.com.duosdevelop.ecommerce.repositories.StateRepository;
-
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner{
-
-	@Autowired
-	private CategoryRepository repositoryCategory;
-	
-	@Autowired
-	private ProductRepository repositoryProduct;
-	
-	@Autowired
-	private StateRepository repositoryState;
-	
-	@Autowired
-	private CityRepository repositoryCity;
-	
-	@Autowired
-	private CustomerRepository repositoryCustomer;
-	
-	@Autowired
-	private AddressRepository repositoryAddress;
-	
-	@Autowired
-	private PedidoRepository repositoryPedido;
-	
-	@Autowired
-	private PaymentRepository repositoryPayment;
-	
-	@Autowired
-	private ItemPedidoRepository repositoryItemPedido;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
@@ -68,100 +14,6 @@ public class EcommerceApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		Category cat1 = new Category(null, "Informática");
-		Category cat2 = new Category(null, "Escritório");
-		Category cat3 = new Category(null, "Cama mesa e banho");
-		Category cat4 = new Category(null, "Eletrônicos");
-		Category cat5 = new Category(null, "Jardinagem");
-		Category cat6 = new Category(null, "Decoração");
-		Category cat7 = new Category(null, "Perfumaria");
-		
-		Product prod1 = new Product(null, "Computador", 2000.00);
-		Product prod2 = new Product(null, "Impressora", 800.00);
-		Product prod3 = new Product(null, "Mouse", 80.00);
-		Product prod4 = new Product(null, "Mesa de Escritório", 300.00);
-		Product prod5 = new Product(null, "Toalha", 50.00);
-		Product prod6 = new Product(null, "Colcha", 200.00);
-		Product prod7 = new Product(null, "TV true color", 1200.00);
-		Product prod8 = new Product(null, "Roçadeira", 800.00);
-		Product prod9 = new Product(null, "Abajour", 100.00);
-		Product prod10 = new Product(null, "Pendente", 180.00);
-		Product prod11 = new Product(null, "Shampoo", 90.00);
-		
-		State mg = new State(null, "Minas Gerais");
-		State sp = new State(null, "São Paulo");
-		
-		City ubCity = new City(null, "Uberlândia", mg);
-		City spCity = new City(null, "São Paulo", sp);
-		City cmCity = new City(null, "Campinas", sp);
-		
-		Customer customer1 = new Customer(null, "Maria das Dores", "maria@gmail.com", "123.456.789-12", TypeCustomer.PESSOA_FISICA);
-		customer1.getTel().addAll(Arrays.asList("24351435", "53126355"));
-		
-		Address address1 = new Address(null, "Rua 1", "300", "casa", "Jardim", "03297266", customer1, ubCity);
-		Address address2 = new Address(null, "Rua 2", "400", "casa 3", "Jardim", "63429628", customer1, spCity);
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), customer1, address1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), customer1, address2);
-		
-		Payment payment1 = new CardPayment(null, StatePayment.QUITADO, ped1, 6);
-		ped1.setPayment(payment1);
-		
-		Payment payment2 = new PaymentSlip(null, StatePayment.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		ped2.setPayment(payment2);
-		
-		ItemPedido item1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.00);
-		
-		ItemPedido item2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
-		
-		ItemPedido item3 = new ItemPedido(ped2, prod2, 100.00, 1, 800.00);
-		
-		customer1.getAddress().addAll(Arrays.asList(address1, address2));
-		
-		cat1.getProducts().addAll(Arrays.asList(prod1, prod2, prod3));
-		cat2.getProducts().addAll(Arrays.asList(prod2, prod4));
-		cat3.getProducts().addAll(Arrays.asList(prod5, prod6));
-		cat4.getProducts().addAll(Arrays.asList(prod1, prod2, prod3, prod7));
-		cat5.getProducts().addAll(Arrays.asList(prod8));
-		cat6.getProducts().addAll(Arrays.asList(prod9, prod10));
-		cat7.getProducts().addAll(Arrays.asList(prod11));
-		
-		prod1.getCategories().addAll(Arrays.asList(cat1, cat4));
-		prod2.getCategories().addAll(Arrays.asList(cat1, cat2, cat4));
-		prod3.getCategories().addAll(Arrays.asList(cat1, cat4));
-		prod4.getCategories().addAll(Arrays.asList(cat2));
-		prod5.getCategories().addAll(Arrays.asList(cat3));
-		prod6.getCategories().addAll(Arrays.asList(cat3));
-		prod7.getCategories().addAll(Arrays.asList(cat4));
-		prod8.getCategories().addAll(Arrays.asList(cat5));
-		prod9.getCategories().addAll(Arrays.asList(cat6));
-		prod10.getCategories().addAll(Arrays.asList(cat6));
-		prod11.getCategories().addAll(Arrays.asList(cat7));
-		
-		mg.getCities().addAll(Arrays.asList(ubCity));
-		sp.getCities().addAll(Arrays.asList(spCity, cmCity));
-		
-		customer1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		ped1.getItens().addAll(Arrays.asList(item1, item2));
-		ped2.getItens().addAll(Arrays.asList(item3));
-		
-		prod1.getItens().addAll(Arrays.asList(item1));
-		prod2.getItens().addAll(Arrays.asList(item3));
-		prod3.getItens().addAll(Arrays.asList(item2));
-		
-		repositoryCategory.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		repositoryProduct.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10, prod11));
-		repositoryState.saveAll(Arrays.asList(mg, sp));
-		repositoryCity.saveAll(Arrays.asList(ubCity, spCity, cmCity));
-		repositoryCustomer.saveAll(Arrays.asList(customer1));
-		repositoryAddress.saveAll(Arrays.asList(address1, address2));
-		
-		repositoryPedido.saveAll(Arrays.asList(ped1, ped2));
-		repositoryPayment.saveAll(Arrays.asList(payment1, payment2));
-		
-		repositoryItemPedido.saveAll(Arrays.asList(item1, item2, item3));
 	}
 
 }
---
+++ src/main/java/br/com/duosdevelop/ecommerce/services/DBService.java
@@ -0,0 +1,140 @@
+package br.com.duosdevelop.ecommerce.services;
+
+import br.com.duosdevelop.ecommerce.domain.*;
+import br.com.duosdevelop.ecommerce.domain.enums.StatePayment;
+import br.com.duosdevelop.ecommerce.domain.enums.TypeCustomer;
+import br.com.duosdevelop.ecommerce.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private CategoryRepository repositoryCategory;
+
+    @Autowired
+    private ProductRepository repositoryProduct;
+
+    @Autowired
+    private StateRepository repositoryState;
+
+    @Autowired
+    private CityRepository repositoryCity;
+
+    @Autowired
+    private CustomerRepository repositoryCustomer;
+
+    @Autowired
+    private AddressRepository repositoryAddress;
+
+    @Autowired
+    private PedidoRepository repositoryPedido;
+
+    @Autowired
+    private PaymentRepository repositoryPayment;
+
+    @Autowired
+    private ItemPedidoRepository repositoryItemPedido;
+
+    public void instantiateTestDatabase() throws ParseException {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+        Category cat3 = new Category(null, "Cama mesa e banho");
+        Category cat4 = new Category(null, "Eletrônicos");
+        Category cat5 = new Category(null, "Jardinagem");
+        Category cat6 = new Category(null, "Decoração");
+        Category cat7 = new Category(null, "Perfumaria");
+
+        Product prod1 = new Product(null, "Computador", 2000.00);
+        Product prod2 = new Product(null, "Impressora", 800.00);
+        Product prod3 = new Product(null, "Mouse", 80.00);
+        Product prod4 = new Product(null, "Mesa de Escritório", 300.00);
+        Product prod5 = new Product(null, "Toalha", 50.00);
+        Product prod6 = new Product(null, "Colcha", 200.00);
+        Product prod7 = new Product(null, "TV true color", 1200.00);
+        Product prod8 = new Product(null, "Roçadeira", 800.00);
+        Product prod9 = new Product(null, "Abajour", 100.00);
+        Product prod10 = new Product(null, "Pendente", 180.00);
+        Product prod11 = new Product(null, "Shampoo", 90.00);
+
+        State mg = new State(null, "Minas Gerais");
+        State sp = new State(null, "São Paulo");
+
+        City ubCity = new City(null, "Uberlândia", mg);
+        City spCity = new City(null, "São Paulo", sp);
+        City cmCity = new City(null, "Campinas", sp);
+
+        Customer customer1 = new Customer(null, "Maria das Dores", "maria@gmail.com", "123.456.789-12", TypeCustomer.PESSOA_FISICA);
+        customer1.getTel().addAll(Arrays.asList("24351435", "53126355"));
+
+        Address address1 = new Address(null, "Rua 1", "300", "casa", "Jardim", "03297266", customer1, ubCity);
+        Address address2 = new Address(null, "Rua 2", "400", "casa 3", "Jardim", "63429628", customer1, spCity);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), customer1, address1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), customer1, address2);
+
+        Payment payment1 = new CardPayment(null, StatePayment.QUITADO, ped1, 6);
+        ped1.setPayment(payment1);
+
+        Payment payment2 = new PaymentSlip(null, StatePayment.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPayment(payment2);
+
+        ItemPedido item1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.00);
+
+        ItemPedido item2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
+
+        ItemPedido item3 = new ItemPedido(ped2, prod2, 100.00, 1, 800.00);
+
+        customer1.getAddress().addAll(Arrays.asList(address1, address2));
+
+        cat1.getProducts().addAll(Arrays.asList(prod1, prod2, prod3));
+        cat2.getProducts().addAll(Arrays.asList(prod2, prod4));
+        cat3.getProducts().addAll(Arrays.asList(prod5, prod6));
+        cat4.getProducts().addAll(Arrays.asList(prod1, prod2, prod3, prod7));
+        cat5.getProducts().addAll(Arrays.asList(prod8));
+        cat6.getProducts().addAll(Arrays.asList(prod9, prod10));
+        cat7.getProducts().addAll(Arrays.asList(prod11));
+
+        prod1.getCategories().addAll(Arrays.asList(cat1, cat4));
+        prod2.getCategories().addAll(Arrays.asList(cat1, cat2, cat4));
+        prod3.getCategories().addAll(Arrays.asList(cat1, cat4));
+        prod4.getCategories().addAll(Arrays.asList(cat2));
+        prod5.getCategories().addAll(Arrays.asList(cat3));
+        prod6.getCategories().addAll(Arrays.asList(cat3));
+        prod7.getCategories().addAll(Arrays.asList(cat4));
+        prod8.getCategories().addAll(Arrays.asList(cat5));
+        prod9.getCategories().addAll(Arrays.asList(cat6));
+        prod10.getCategories().addAll(Arrays.asList(cat6));
+        prod11.getCategories().addAll(Arrays.asList(cat7));
+
+        mg.getCities().addAll(Arrays.asList(ubCity));
+        sp.getCities().addAll(Arrays.asList(spCity, cmCity));
+
+        customer1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+        ped1.getItens().addAll(Arrays.asList(item1, item2));
+        ped2.getItens().addAll(Arrays.asList(item3));
+
+        prod1.getItens().addAll(Arrays.asList(item1));
+        prod2.getItens().addAll(Arrays.asList(item3));
+        prod3.getItens().addAll(Arrays.asList(item2));
+
+        repositoryCategory.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+        repositoryProduct.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10, prod11));
+        repositoryState.saveAll(Arrays.asList(mg, sp));
+        repositoryCity.saveAll(Arrays.asList(ubCity, spCity, cmCity));
+        repositoryCustomer.saveAll(Arrays.asList(customer1));
+        repositoryAddress.saveAll(Arrays.asList(address1, address2));
+
+        repositoryPedido.saveAll(Arrays.asList(ped1, ped2));
+        repositoryPayment.saveAll(Arrays.asList(payment1, payment2));
+
+        repositoryItemPedido.saveAll(Arrays.asList(item1, item2, item3));
+    }
+}
