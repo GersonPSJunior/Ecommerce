5568c972c60008fb7bdc8c13ce4e8ccd5dd0080c
Gerson Junior <gersonjunior1771@gmail.com>
2019-09-26 14:53:37 -0300

    Email HTML

--- src/main/java/br/com/duosdevelop/ecommerce/services/EmailService.java
+++ src/main/java/br/com/duosdevelop/ecommerce/services/EmailService.java
@@ -3,8 +3,13 @@ package br.com.duosdevelop.ecommerce.services;
 import br.com.duosdevelop.ecommerce.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
+
 }
